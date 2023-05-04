a268c59d6cb4026d8d6a63eeb0ed3eff1b7ad7f3
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-05-04 15:08:32 +0100

    Comment Controller mehods

--- Core/Controllers/Controller.php
+++ Core/Controllers/Controller.php
@@ -6,8 +6,9 @@ use Facades\Helpers\Common;
 
 class Controller extends Common
 {
+
     /**
-     * retourne une vue
+     * This method returns a view by concatenating the view path with the provided $view string. It checks whether the file exists and, if it does, returns an array with the view path, a context array (if provided) and a title (if provided). If the file does not exist, it returns a 404 view path with global context data.
      * @param string $viewPath
      * @return array full view path or 404 view path with context globale data
      */
@@ -24,13 +25,16 @@ class Controller extends Common
         ];
     }
 
-    protected function can(array $user_type = null)
+    /**
+     * This method checks whether the user is authenticated by calling the mustAuthenticate() method. If the user is authenticated, it checks whether the user's role is included in the $user_type array (or defaults to ['Admin']). If the user's role matches one of the roles in $user_type, the method returns nothing. If the user's role does not match any of the roles in $user_type, the method redirects back and returns an error message.
+     */
+    protected function can(array $user_type = null, string $column = 'roles')
     {
         $this->mustAuthenticate();
 
-        $user_type = $user_type ?? ['Admin', 'Technicien', 'Client'];
+        $user_type = $user_type ?? ['Admin'];
 
-        $user_role = $this->user('roles');
+        $user_role = $this->user($column);
 
         foreach ($user_type as $role) {
             if ($user_role === $role) return;
@@ -39,6 +43,9 @@ class Controller extends Common
         return $this->back()->with("Access Denied to this Ressource");
     }
 
+    /**
+     * This method checks whether the user is authenticated by checking if the user array is not empty. If $statut is true, it returns nothing if the user is authenticated and an error message if the user is not authenticated. If $statut is false, it returns nothing if the user is not authenticated and an error message if the user is authenticated. It uses the back() method to redirect the user back to the previous page.
+     */
     protected function mustAuthenticate(bool $statut = true)
     {
         if ($statut) {
