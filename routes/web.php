e691d9782dc01757928015e05cfbd9dce98334a7
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-05-04 18:19:08 +0100

    Config Route namespace

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 namespace Routes;
 
 use App\Controllers\WelcomeController;
-use Facades\Routes\Route;
+use Juste\Facades\Routes\Route;
 
 Route::get("/", [WelcomeController::class, 'welcome'])->name('welcome');
--- views/index.php
+++ views/index.php
@@ -6,10 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle . ' | ' ?> Bravo</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
+    <link rel="favicon" type="image/ico" href="/favicon.ico">
 </head>
 
 <body>
