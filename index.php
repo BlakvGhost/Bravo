868ca11fcccdba4943f339fb17a30b38f71e427a
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-07-10 02:49:32 +0100

    Moving index script to public directory for security reasons

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once  'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-require_once 'bootstrap/bootstrap.php';
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+define('ROOT', __DIR__ . '/../');
+
+require_once  ROOT . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once ROOT . '/bootstrap/bootstrap.php';
