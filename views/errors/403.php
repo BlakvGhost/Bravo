5156c31cd326d7177a5a47321dbebea6ae5c3d89
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-07-10 03:20:52 +0100

    Updated Error Page Template for responsive reasons

--- views/errors/403.php
+++ views/errors/403.php
@@ -1,2 +1,17 @@
-<h2 style="color: #f0c0c0; text-align:center; margin: 2% auto">Error 403</h2>
-<h1 style="color: red; text-align:center; margin: auto">Vue Not Exist</h1>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VUE NOT EXIST ERROR | Bravo</title>
+    <link rel="favicon" type="image/ico" href="/favicon.ico">
+</head>
+
+<body>
+    <h2 style="color: #f0c0c0; text-align:center; margin: 2% auto">Error 403</h2>
+    <h1 style="color: red; text-align:center; margin: auto">Vue Not Exist</h1>
+</body>
+
+</html>
--- views/errors/404.php
+++ views/errors/404.php
@@ -1,2 +1,17 @@
-<h2 style="color: #f0c0c0; text-align:center; margin: 2% auto">Error 404</h2>
-<h1 style="color: red; text-align:center; margin: auto">Page Not Found</h1>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAGE NOT FOUND ERROR | Bravo</title>
+    <link rel="favicon" type="image/ico" href="/favicon.ico">
+</head>
+
+<body>
+    <h2 style="color: #f0c0c0; text-align:center; margin: 2% auto">Error 404</h2>
+    <h1 style="color: red; text-align:center; margin: auto">Page Not Found</h1>
+</body>
+
+</html>
